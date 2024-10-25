4fc93d0168b8cfd39a81049a1039a7124338ad72
Oxamyt <dentez.danil@gmail.com>
2024-10-25 20:39:47 +0300

    create PhotoDisplay component with targeting box

--- src/components/PhotoDisplay.jsx
+++ src/components/PhotoDisplay.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
 function PhotoDisplay() {
-  return <img src="../../public/game-picture.jpg" alt="game image"></img>;
+  const [boxPosition, setBoxPosition] = useState(null);
+
+  const handleImageClick = (e) => {
+    const { clientX, clientY } = e;
+    const boxWidth = 100;
+    const boxHeight = 100;
+
+    const imgRect = e.target.getBoundingClientRect();
+    const adjustedX = clientX - imgRect.left - boxWidth / 2;
+    const adjustedY = clientY - imgRect.top - boxHeight / 2;
+
+    setBoxPosition({
+      x: adjustedX,
+      y: adjustedY,
+    });
+  };
+
+  return (
+    <div className="relative">
+      <img
+        src="../../public/game-picture.jpg"
+        alt="game image"
+        className="w-full h-full cursor-pointer z-10"
+        onClick={handleImageClick}
+      />
+      {boxPosition && (
+        <div
+          data-testid="targeting-box"
+          className="absolute border-2 rounded-full border-red-500"
+          style={{
+            left: boxPosition.x,
+            top: boxPosition.y,
+            width: `100px`,
+            height: `100px`,
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default PhotoDisplay;
--- src/tests/PhotoDisplay.test.jsx
+++ src/tests/PhotoDisplay.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import PhotoDisplay from "../components/PhotoDisplay.jsx";
 
@@ -7,5 +7,17 @@ describe("PhotoDisplay", () => {
     render(<PhotoDisplay />);
     const image = screen.getByAltText("game image");
     expect(image).toBeInTheDocument();
+  });
+
+  it("displays targeting box on image click", () => {
+    render(<PhotoDisplay />);
+
+    const image = screen.getByAltText("game image");
+    fireEvent.click(image, { clientX: 100, clientY: 100 });
+
+    const targetingBox = screen.getByTestId("targeting-box");
+    expect(targetingBox).toBeInTheDocument();
+    expect(targetingBox.style.left).toBe("50px");
+    expect(targetingBox.style.top).toBe("50px");
   });
 });
