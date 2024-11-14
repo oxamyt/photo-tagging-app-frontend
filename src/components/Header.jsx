220017bf99894f6795b33ada42a2ddeed05ea142
oxamyt <dentez.danil@gmail.com>
2024-11-14 17:21:07 +0200

    render character dropdown conditionally in header for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Timer from "./common/Timer";
+import { useState } from "react";
 
 function Header({
   gameStarted,
@@ -10,9 +11,13 @@ function Header({
   gameOver,
   renderTimer,
 }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+
   return (
-    <header className="sticky top-0 z-20 text-xl lg:text-3xl max-w-full flex justify-between items-center text-[#d1d9e0] bg-[#1c1f26] p-6 shadow-md ">
-      <Link to={"/"} className="font-bold ">
+    <header className="sticky top-0 z-20 text-xl lg:text-3xl max-w-full flex justify-between items-center text-[#d1d9e0] bg-[#1c1f26] p-6 shadow-md">
+      <Link to={"/"} className="font-bold">
         Photo Tagging App
       </Link>
 
@@ -24,14 +29,40 @@ function Header({
           gameOver={gameOver}
         />
       ) : (
-        <Link to={"/leaderboard"} className=" font-bold ">
+        <Link to={"/leaderboard"} className="font-bold">
           Leaderboard
         </Link>
       )}
 
       {characters && (
-        <section>
-          <ul className="flex font-bold gap-2 justify-center items-center">
+        <section className="relative">
+          <button
+            onClick={toggleDropdown}
+            className="lg:hidden font-bold bg-gray-800 text-white py-2 px-4 rounded-md"
+          >
+            Characters
+          </button>
+
+          <ul
+            className={`${
+              isDropdownOpen ? "block" : "hidden"
+            } absolute right-0 mt-2 py-2 w-48 bg-gray-800 text-white rounded-lg shadow-lg lg:hidden`}
+          >
+            {characters.map((character) => (
+              <li
+                key={character.name}
+                style={{
+                  color: character.found ? "green" : "yellow",
+                }}
+                aria-label={character.name}
+                className="px-4 py-2"
+              >
+                {character.name}
+              </li>
+            ))}
+          </ul>
+
+          <ul className="hidden lg:flex gap-2 font-bold">
             {characters.map((character) => (
               <li
                 key={character.name}
